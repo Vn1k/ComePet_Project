fc6f2a40a54a5c3bf66d2c3e110aa60c481507a3
Vinik <56496329+Vn1k@users.noreply.github.com>
2024-10-24 21:49:11 +0700

    FIX GRADLE

--- ComePet_App/app/build.gradle.kts
+++ ComePet_App/app/build.gradle.kts
@@ -41,34 +41,34 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation(libs.androidx.core.ktx.v190)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.livedata.ktx.v262)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx.v262)
+    implementation(libs.androidx.navigation.fragment.ktx.v275)
+    implementation(libs.androidx.navigation.ui.ktx.v275)
+    implementation(libs.androidx.legacy.support.v4)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.firebase.database.ktx)
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
     //google
     implementation(libs.google.firebase.auth)
     implementation(libs.google.firebase.firestore)
-    implementation(platform("com.google.firebase:firebase-bom:33.5.0"))
-    implementation ("com.google.android.gms:play-services-auth:20.3.0")
-    implementation ("com.google.firebase:firebase-firestore:25.1.1")
-    implementation("com.google.firebase:firebase-analytics")
-    implementation ("com.google.firebase:firebase-auth:21.0.1")
+    implementation(platform(libs.firebase.bom.v3351))
+    implementation (libs.com.google.firebase.firebase.analytics3)
+    implementation (libs.play.services.auth)
+    implementation (libs.com.google.firebase.firebase.firestore)
+    implementation(libs.com.google.firebase.firebase.analytics2)
+    implementation (libs.com.google.firebase.firebase.auth)
 
     // glide
-    implementation("com.github.bumptech.glide:glide:4.14.2")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
-
-    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation(libs.glide)
+    annotationProcessor(libs.compiler)
+    implementation(libs.play.services.maps)
 
 }
