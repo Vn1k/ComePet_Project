0e4cf09dd8f168485b6c31848b98528938104692
JoseAndreasLie <jose.lie2208@gmail.com>
2024-10-25 18:56:52 +0700

    Profile Fragments done, anak2nya blom tapi :>

---
+++ ComePet_App/app/src/main/java/com/example/comepet/ui/profile/PetData.kt
@@ -0,0 +1,8 @@
+package com.example.comepet.ui.profile
+
+data class PetData(
+    val id: Int,
+    val name: String,
+    val age: String,
+    val imageUrl: String
+)
--- ComePet_App/app/src/main/java/com/example/comepet/ui/profile/subfragments/ProfileShelterFragment.kt
+++ ComePet_App/app/src/main/java/com/example/comepet/ui/profile/subfragments/ProfileShelterFragment.kt
@@ -13,44 +13,44 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class ProfileShelterFragment : Fragment(), OnMapReadyCallback {
+class ProfileShelterFragment : Fragment(){
 
-    private lateinit var mapView: MapView
+//    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile_shelter, container, false)
-        mapView = view.findViewById(R.id.mapView)
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
+//        mapView = view.findViewById(R.id.mapView)
+//        mapView.onCreate(savedInstanceState)
+//        mapView.getMapAsync(this)
         return view
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        val location = LatLng(-34.0, 151.0) // Replace with your desired location
-        googleMap.addMarker(MarkerOptions().position(location).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 10f))
-    }
+//    override fun onMapReady(googleMap: GoogleMap) {
+//        val location = LatLng(-34.0, 151.0) // Replace with your desired location
+//        googleMap.addMarker(MarkerOptions().position(location).title("Marker in Sydney"))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 10f))
+//    }
 
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
-    }
+//    override fun onResume() {
+//        super.onResume()
+//        mapView.onResume()
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        mapView.onPause()
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        mapView.onDestroy()
+//    }
+//
+//    override fun onLowMemory() {
+//        super.onLowMemory()
+//        mapView.onLowMemory()
+//    }
 }
