d438cdfa3d2710d42f89ea77fd13e51bb3043e21
Vinik <56496329+Vn1k@users.noreply.github.com>
2024-10-25 17:06:18 +0700

    splash screen

--- ComePet_App/app/src/main/java/com/example/comepet/MainActivity.kt
+++ ComePet_App/app/src/main/java/com/example/comepet/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.comepet
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnticipateInterpolator
+import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,9 +20,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navView: BottomNavigationView
-
+    private val MainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        splashScreen.apply {
+            setKeepOnScreenCondition{
+                !MainViewModel.isReady.value
+            }
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
