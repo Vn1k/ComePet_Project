798601efcf992db17b4a7406d378acf9754ada56
WarmHatori <warmhatori@gmail.com>
2024-10-25 16:10:34 +0700

    Edit Pet

--- ComePet_App/app/src/main/java/com/example/comepet/ui/home/HomeFragment.kt
+++ ComePet_App/app/src/main/java/com/example/comepet/ui/home/HomeFragment.kt
@@ -10,7 +10,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.comepet.R
 
-
 class HomeFragment : Fragment() {
 
     private val homeViewModel: HomeViewModel by viewModels()
