7267801b9a4f58c2fd150944bfb8ef070ccc450b
JoseAndreasLie <jose.lie2208@gmail.com>
2024-11-19 13:12:56 +0700

    Edit Profile -banyak
    Pilih foto blom bisa

--- ComePet_App/app/src/main/java/com/example/comepet/ui/profile/subfragments/ProfileEditFragment.kt
+++ ComePet_App/app/src/main/java/com/example/comepet/ui/profile/subfragments/ProfileEditFragment.kt
@@ -1,23 +1,39 @@
 package com.example.comepet.ui.profile.subfragments
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.comepet.R
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 
 class ProfileEditFragment : Fragment() {
     private lateinit var saveButton: Button
+    private lateinit var editTextName: EditText
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextPhone: EditText
+    private lateinit var editTextBio: EditText
+    private lateinit var editTextLocation: EditText
+    private lateinit var cameraButton: ImageButton
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+        mAuth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
     }
 
     override fun onCreateView(
@@ -32,7 +48,81 @@ class ProfileEditFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         saveButton = view.findViewById(R.id.buttonSave)
         saveButton.setOnClickListener {
+            saveUserData()
             findNavController().navigate(R.id.navigation_edit_profile_to_navigation_profile)
+        }
+        cameraButton.setOnClickListener {
+
+        }
+
+        editTextName = view.findViewById(R.id.editTextName)
+        editTextUsername = view.findViewById(R.id.editTextUsername)
+        editTextPhone = view.findViewById(R.id.editTextPhone)
+        editTextBio = view.findViewById(R.id.editTextBio)
+        editTextLocation = view.findViewById(R.id.editTextLocation)
+        getCurrentUserData()
+
+    }
+
+    private fun getCurrentUserData() {
+        val currentUser = mAuth.currentUser
+
+        if (currentUser != null) {
+            db.collection("users")
+                .document(currentUser.uid)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null && document.exists()) {
+                        // Retrieve the fields
+                        val name = document.getString("name") ?: ""
+                        val username = document.getString("username") ?: ""
+                        val phone = document.getString("phone") ?: ""
+                        val bio = document.getString("bio") ?: ""
+                        val location = document.getString("location") ?: ""
+                        val profilePicture = document.getString("profilePicture") ?: ""
+
+                        // Update the UI
+                        editTextName.setText(name)
+                        editTextUsername.setText(username)
+                        editTextPhone.setText(phone)
+                        editTextBio.setText(bio)
+                        editTextLocation.setText(location)
+
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("ProfileFragment", "Error getting user data: ", exception)
+                }
+        }
+    }
+
+    private fun saveUserData(){
+        val currentUser = mAuth.currentUser
+
+        if (currentUser != null) {
+            val name = editTextName.text.toString()
+            val username = editTextUsername.text.toString()
+            val phone = editTextPhone.text.toString()
+            val bio = editTextBio.text.toString()
+            val location = editTextLocation.text.toString()
+
+            val user = hashMapOf(
+                "name" to name,
+                "username" to username,
+                "phone" to phone,
+                "bio" to bio,
+                "location" to location
+            )
+
+            db.collection("users")
+                .document(currentUser.uid)
+                .set(user)
+                .addOnSuccessListener {
+                    Log.d("ProfileEditFragment", "User data saved")
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("ProfileEditFragment", "Error saving user data: ", exception)
+                }
         }
     }
 
